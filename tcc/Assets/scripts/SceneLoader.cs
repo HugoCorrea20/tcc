160ffb9f6a0a22e8f7285b9a67f4a96609423f35
HugoCorrea20 <61765713+HugoCorrea20@users.noreply.github.com>
2024-02-21 10:36:17 -0300

    inicio do prototico do loading

---
+++ tcc/Assets/scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    
+    [Header("Porcetagem")]
+    public Slider Slloading;
+    public Text textporcetagem;
+    [Header("Imagem")]
+    public Image imgparaMudar;
+    public Sprite[] imagens;
+
+}

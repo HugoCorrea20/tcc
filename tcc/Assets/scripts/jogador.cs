c73019c56ae25ad84c841f909df3dfbe7517015d
HugoCorrea20 <61765713+HugoCorrea20@users.noreply.github.com>
2024-03-01 08:29:18 -0300

    protótipo tiro pronto

--- tcc/Assets/scripts/jogador.cs
+++ tcc/Assets/scripts/jogador.cs
@@ -6,6 +6,8 @@ public class jogador : MonoBehaviour
 {
     public float speed = 5f; // Velocidade de movimento do personagem
     public float jumpForce = 10f; // Força do pulo
+    public GameObject bulletPrefab; // Prefab da bala
+    public float velocidade_tiro = 10f;
 
     private Rigidbody2D rb; // Referência ao componente Rigidbody2D
     private bool isGrounded; // Verifica se o jogador está no chão
@@ -33,6 +35,29 @@ public class jogador : MonoBehaviour
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false; // O jogador já não está mais no chão após o pulo
         }
+
+        // Verifica se o botão direito do mouse foi pressionado
+        if (Input.GetMouseButtonDown(1))
+        {
+            Shoot(moveHorizontal); // Chama a função de atirar e passa a direção do movimento
+        }
+    }
+
+    // Função para atirar
+    void Shoot(float direction)
+    {
+        // Instancia o objeto de bala na posição do jogador
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+        // Determina a direção do tiro com base na direção do movimento do jogador
+        Vector2 shootDirection = Vector2.right; // Por padrão, o tiro vai para a direita
+        if (direction < 0) // Se o jogador estiver se movendo para a esquerda
+        {
+            shootDirection = Vector2.left; // Muda a direção do tiro para a esquerda
+        }
+
+        // Aplica uma força na direção determinada à bala
+        bullet.GetComponent<Rigidbody2D>().AddForce(shootDirection * velocidade_tiro, ForceMode2D.Impulse);
     }
 
     // Verifica se o jogador está colidindo com o chão

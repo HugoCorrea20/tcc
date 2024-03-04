1a2a7f71b0dc9cdcc9276cdf52b4368c25899c7f
HugoCorrea20 <61765713+HugoCorrea20@users.noreply.github.com>
2024-03-04 09:35:01 -0300

    tiro resolvido e inimigo perdendo a vida

--- tcc/Assets/scripts/inimigo.cs
+++ tcc/Assets/scripts/inimigo.cs
@@ -3,14 +3,13 @@ using UnityEngine;
 public class inimigo : MonoBehaviour
 {
     public int maxHealth = 100;
-    private int currentHealth;
+    public  int currentHealth;
 
     void Start()
     {
         currentHealth = maxHealth;
     }
 
-    // Método para causar dano ao inimigo
     public void TakeDamage(int damageAmount)
     {
         currentHealth -= damageAmount;
@@ -21,32 +20,18 @@ public class inimigo : MonoBehaviour
         }
     }
 
-    // Método para lidar com a morte do inimigo
     void Die()
     {
-        // Adicione aqui qualquer lógica que você queira executar quando o inimigo morrer,
-        // como reproduzir uma animação de morte, pontuação do jogador, etc.
         Destroy(gameObject);
     }
 
-    // Verifica colisões com balas
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D other)
     {
-        if (collision.gameObject.CompareTag("Bala"))
+        if (other.gameObject.CompareTag("Bala"))
         {
-            // Obtém o componente Bala do objeto colidido
-            Bala bala = collision.gameObject.GetComponent<Bala>();
-
-            // Verifica se o componente Bala foi encontrado
-            if (bala != null)
-            {
-                // Chama a função TakeDamage para causar dano ao inimigo
-                TakeDamage(bala.dano);
-
-                // Destroi a bala após atingir o inimigo
-                Destroy(collision.gameObject);
-            }
+          
+            Destroy(other.gameObject); 
+            TakeDamage(10);
         }
     }
-
 }
--- tcc/Assets/scripts/jogador.cs
+++ tcc/Assets/scripts/jogador.cs
@@ -4,73 +4,77 @@ using UnityEngine;
 
 public class jogador : MonoBehaviour
 {
-    public float speed = 5f; // Velocidade de movimento do personagem
-    public float jumpForce = 10f; // Força do pulo
-    public GameObject bulletPrefab; // Prefab da bala
+    public float speed = 5f; 
+    public float jumpForce = 10f; 
+    public GameObject bulletPrefab; 
     public float velocidade_tiro = 10f;
-    public Transform bulletSpawnPointRight; // Ponto de spawn da bala quando o jogador está indo para a direita
-    public Transform bulletSpawnPointLeft; // Ponto de spawn da bala quando o jogador está indo para a esquerda
-    private Rigidbody2D rb; // Referência ao componente Rigidbody2D
-    private bool isGrounded; // Verifica se o jogador está no chão
-
+    public Transform bulletSpawnPointRight; 
+    public Transform bulletSpawnPointLeft; 
+    private Rigidbody2D rb; 
+    private bool isGrounded;
+    private float lastDirection = 1f;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); // Obtém o componente Rigidbody2D do objeto
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        // Captura os inputs de movimento
+        
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-        // Calcula a direção do movimento
+        
         Vector2 movement = new Vector2(moveHorizontal, 0f);
 
-        // Aplica o movimento ao Rigidbody2D
+
         rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
 
-        // Verifica se o jogador está no chão
+       
+        if (moveHorizontal != 0)
+        {
+            lastDirection = Mathf.Sign(moveHorizontal);
+        }
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-            // Aplica uma força vertical (para cima) ao Rigidbody2D
+           
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            isGrounded = false; // O jogador já não está mais no chão após o pulo
+            isGrounded = false;
         }
 
-        // Verifica se o botão direito do mouse foi pressionado
+       
         if (Input.GetMouseButtonDown(1))
         {
-            Shoot(moveHorizontal); // Chama a função de atirar e passa a direção do movimento
+            Shoot(); 
         }
     }
 
-    // Função para atirar
-    void Shoot(float direction)
+
+    void Shoot()
     {
-        // Ajusta a posição de spawn do tiro de acordo com a direção do jogador
+        
         Vector3 spawnPosition;
 
-        if (direction > 0) // Jogador está indo para a direita
+        if (lastDirection > 0) 
         {
             spawnPosition = bulletSpawnPointRight.position;
         }
-        else // Jogador está indo para a esquerda
+        else
         {
             spawnPosition = bulletSpawnPointLeft.position;
         }
 
-        // Instancia o objeto de bala na posição de spawn determinada
+       
         GameObject bullet = Instantiate(bulletPrefab, spawnPosition, Quaternion.identity);
 
-        // Determina a direção do tiro com base na direção do movimento do jogador
-        Vector2 shootDirection = direction > 0 ? Vector2.right : Vector2.left;
+       
+        Vector2 shootDirection = lastDirection > 0 ? Vector2.right : Vector2.left;
 
-        // Aplica uma força na direção determinada à bala
+        
         bullet.GetComponent<Rigidbody2D>().AddForce(shootDirection * velocidade_tiro, ForceMode2D.Impulse);
     }
 
-    // Verifica se o jogador está colidindo com o chão
+  
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Chao"))
@@ -80,7 +84,6 @@ public class jogador : MonoBehaviour
 
     }
 
-    // Verifica se o jogador não está mais colidindo com o chão
     void OnCollisionExit2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Chao"))

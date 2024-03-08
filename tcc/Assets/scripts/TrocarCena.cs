520301e5d7d486c4b3403b1340953fa80aa4b1ff
HugoCorrea20 <61765713+HugoCorrea20@users.noreply.github.com>
2024-03-08 09:35:40 -0300

    introdução pronto

--- tcc/Assets/scripts/TrocarCena.cs
+++ tcc/Assets/scripts/TrocarCena.cs
@@ -9,6 +9,14 @@ public class TrocarCena : MonoBehaviour
     public string nomeDaCena;
     public GameObject creditos;
     public GameObject menuprincipal;
+    public GameObject introdução;
+    public GameObject texto1;
+    public GameObject texto2;
+    public GameObject texto3;
+    public GameObject começarbtn;
+    public GameObject proximofalar1btn;
+    public GameObject proximofalar2btn;
+   
 
     public void TrocarParaCena()
     {
@@ -30,4 +38,27 @@ public class TrocarCena : MonoBehaviour
         creditos.SetActive(false );
         menuprincipal.SetActive(true );
     }
+    public void abririntrodução()
+    {
+        menuprincipal.SetActive(false );
+        introdução.SetActive(true);
+        texto1.SetActive(true);
+        começarbtn.SetActive(false);
+        proximofalar1btn.SetActive(true);
+    }
+    public void falar2()
+    {
+        texto2.SetActive(true);
+        texto1.SetActive(false);
+        proximofalar1btn.SetActive(false);
+        proximofalar2btn.SetActive(true);
+    }
+    public void falar3() 
+    { 
+    texto3 .SetActive(true);
+    texto2 .SetActive(false);
+    começarbtn .SetActive(true);
+    proximofalar2btn .SetActive(false);
+    }
+
 }

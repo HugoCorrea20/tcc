ad83fcba05459bf306953f19ae16a36027476d4b
HugoCorrea20 <61765713+HugoCorrea20@users.noreply.github.com>
2024-04-10 09:47:47 -0300

    inimigo colocados da cena 1

--- tcc/Assets/scripts/MovimentoNavio.cs
+++ tcc/Assets/scripts/MovimentoNavio.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MovimentoNavio : MonoBehaviour
 {
     public float velocidadeMovimento = 5f; // Velocidade de movimento do navio
+    public int maxHealth = 100; // Vida máxima do jogador
+    public int currentHealth; // Vida atual do jogador
     public GameObject balaPrefab; // Prefab da bala
     public Transform pontoDeSpawn; // Ponto de spawn da bala
     public float velocidadeBala = 10f; // Velocidade da bala
 
     private float tempoUltimoTiro;
     private bool primeiroTiroDisparado = false;
+    public float tempotiro = 3f;
+    public int danorecibindo = 10;
 
     void Start()
     {
         tempoUltimoTiro = Time.time;
+        currentHealth = maxHealth; // Inicialize a vida atual com a vida máxima
     }
 
     void Update()
@@ -21,7 +27,7 @@ public class MovimentoNavio : MonoBehaviour
         {
             MovimentarNavio();
 
-            if (!primeiroTiroDisparado || Time.time - tempoUltimoTiro >= 3f)
+            if (!primeiroTiroDisparado || Time.time - tempoUltimoTiro >= tempotiro)
             {
                 if (Input.GetMouseButtonDown(0))
                 {
@@ -44,5 +50,33 @@ public class MovimentoNavio : MonoBehaviour
     {
         GameObject bala = Instantiate(balaPrefab, pontoDeSpawn.position, pontoDeSpawn.rotation);
         bala.GetComponent<Rigidbody2D>().velocity = transform.right * velocidadeBala;
+    }
+
+    
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+   
+    void Die()
+    {
+        
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("balainimigo"))
+        {
+            Destroy(collision.gameObject); // Destrua a bala inimiga
+            TakeDamage(danorecibindo); // Cause dano ao jogador
+        }
     }
 }
---
+++ tcc/Assets/scripts/NavioInimigo.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavioInimigo : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public GameObject balaPrefab;
+    public Transform pontoDeTiro;
+    public float alcanceDeVisao = 10f;
+    public LayerMask jogadorLayer;
+
+    public float velocidadeMovimento = 2f;
+    public float limiteDireita = 5f;
+    public float limiteEsquerda = -5f;
+
+    private GameObject jogador;
+    private bool movendoDireita = true;
+    private bool atirando = false;
+    public float tempodetiro= 20f;
+    public int danorecibido = 10;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        jogador = GameObject.FindGameObjectWithTag("Player");
+        InvokeRepeating("AtirarSeJogadorVisivel", 0f, 5f);
+    }
+
+    void Update()
+    {
+        if (!atirando) // Continua o movimento apenas se não estiver atirando
+        {
+            MovimentoLateral();
+        }
+    }
+
+    void MovimentoLateral()
+    {
+        if (movendoDireita)
+        {
+            transform.Translate(Vector2.right * velocidadeMovimento * Time.deltaTime);
+            if (transform.localScale.x < 0) // Verifica se a escala é negativa (flip para esquerda)
+            {
+                Flip(); // Se sim, faz o flip para direita
+            }
+        }
+        else
+        {
+            transform.Translate(Vector2.left * velocidadeMovimento * Time.deltaTime);
+            if (transform.localScale.x > 0) // Verifica se a escala é positiva (flip para direita)
+            {
+                Flip(); // Se sim, faz o flip para esquerda
+            }
+        }
+
+        if (transform.position.x >= limiteDireita)
+        {
+            movendoDireita = false;
+        }
+        else if (transform.position.x <= limiteEsquerda)
+        {
+            movendoDireita = true;
+        }
+
+        if (JogadorVisivel())
+        {
+            atirando = true;
+            Invoke("FinalizarTiro", tempodetiro); // Aqui ajuste o tempo de pausa antes de retomar o movimento
+        }
+    }
+
+    void FinalizarTiro()
+    {
+        atirando = false;
+    }
+
+    void AtirarSeJogadorVisivel()
+    {
+        if (JogadorVisivel())
+        {
+            Atirar();
+        }
+    }
+
+    bool JogadorVisivel()
+    {
+        if (jogador == null)
+            return false;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, (jogador.transform.position - transform.position).normalized, alcanceDeVisao, jogadorLayer);
+
+        if (hit.collider != null && hit.collider.CompareTag("Player"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void Atirar()
+    {
+        GameObject novaBala = Instantiate(balaPrefab, pontoDeTiro.position, Quaternion.Euler(0, 0, 0));
+        novaBala.GetComponent<Rigidbody2D>().velocity = pontoDeTiro.right * 10f;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bala"))
+        {
+            Destroy(collision.gameObject);
+            TakeDamage(danorecibido);
+        }
+    }
+
+    void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
